772d8c8df678d6e2cf7c221734a1469411251663
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-01-07 01:22:21 +0530

    fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
+import reactSVG from "vite-plugin-react-svg";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), reactSVG()],
 })
