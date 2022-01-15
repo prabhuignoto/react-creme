99929cdc4f3bf7f439b350c338c43a1180d490c5
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-01-15 09:19:10 +0530

    add sentry

--- packages/documentation/main.tsx
+++ packages/documentation/main.tsx
@@ -3,9 +3,22 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 
-import LogRocket from "logrocket";
-LogRocket.init("v2ukds/react-creme");
+Sentry.init({
+  dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
+
+  // Alternatively, use `process.env.npm_package_version` for a dynamic release version
+  // if your build tool supports it.
+  release: "my-project-name@2.3.12",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <BrowserRouter>
