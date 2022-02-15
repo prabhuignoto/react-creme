9f0a45b0878212729e55d4bbf0eb30800219b68b
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-02-15 09:31:21 +0530

    updating blockquote

--- packages/lib/components/block-quote/block-quote.tsx
+++ packages/lib/components/block-quote/block-quote.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import classNames from 'classnames';
+import React, { useMemo } from 'react';
 import { InfoIcon } from '../../icons';
 import './block-quote.scss';
 
 export interface BlockQuoteProps {
   children: React.ReactNode;
   showInfoIcon?: boolean;
+  size?: 'sm' | 'md' | 'lg';
 }
 
 const BlockQuote: React.FC<BlockQuoteProps> = ({
   children,
   showInfoIcon = true,
+  size = 'sm',
 }) => {
+  const blockQuoteClass = useMemo(
+    () =>
+      classNames('rc-block-quote', {
+        [`rc-block-quote-${size}`]: size,
+      }),
+    []
+  );
+
   return (
-    <div className="rc-block-quote">
+    <div className={blockQuoteClass}>
       <span className="rc-block-quote-icon">
         {showInfoIcon && <InfoIcon />}
       </span>
