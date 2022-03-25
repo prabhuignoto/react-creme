b24a54f5fcd3323b9a98680f2a35c87ea4c643a0
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-03-25 19:14:13 +0530

    fixing accordion and accordion-group stories

--- packages/storybook/stories/Accordion-Group.stories.tsx
+++ packages/storybook/stories/Accordion-Group.stories.tsx
@@ -1,12 +1,21 @@
 import { ComponentMeta } from '@storybook/react';
 import React from 'react';
-import {
-  AutoClosingSections,
-  CustomIcon,
-  Default,
-  InitialState,
-} from '../../documentation/components/accordion-group/widget-variants';
 import { AccordionGroup } from '../../lib/components';
+
+const Para = () => (
+  <p>
+    Sed laoreet neque eget sem varius, et interdum dui venenatis. Suspendisse in
+    faucibus tortor, nec aliquet arcu. Quisque at Proin auctor velit massa,
+    euismod pretium dui euismod in. Pellentesque rhoncus eros id posuere
+    tincidunt. Maecenas quis libero vitae elit consectetur finibus et ac libero.
+    Donec at fermentum lectus. Cras iaculis augue non mauris interdum, vitae
+    pretium mi blandit. Aenean ultrices pellentesque lectus ac faucibus. Morbi
+    tristique vulputate nisi, id porttitor diam egestas a. Suspendisse a tortor
+    suscipit, accumsan massa at, viverra urna. Maecenas vel lectus sodales,
+    dapibus dolor eget, pharetra neque. Nam eleifend id mauris in suscipit. Ut
+    sed risus at mi vulputate rhoncus.
+  </p>
+);
 
 export default {
   component: AccordionGroup,
@@ -20,7 +29,40 @@ export default {
   title: 'Accordion Group',
 } as ComponentMeta<typeof AccordionGroup>;
 
-export const AutoClosing = () => <>{AutoClosingSections}</>;
-export const CustomIconExample = () => <>{CustomIcon}</>;
-export const InitialStateExample = () => <>{InitialState}</>;
-export const DefaultExample = () => <>{Default}</>;
+export const AutoClosing = () => (
+  <AccordionGroup autoClose titles={['View Info 1', 'View Info 2']}>
+    <Para />
+    <Para />
+  </AccordionGroup>
+);
+
+export const CustomIconExample = () => (
+  <AccordionGroup
+    icons={[
+      <i>
+        <img src="https://img.icons8.com/material-sharp/24/000000/plus--v1.png" />
+      </i>,
+      <i>
+        <img src="https://img.icons8.com/material-sharp/24/000000/plus--v1.png" />
+      </i>,
+    ]}
+    titles={['View Info 1', 'View Info 2']}
+  >
+    <Para />
+    <Para />
+  </AccordionGroup>
+);
+
+export const InitialStateExample = () => (
+  <AccordionGroup expanded titles={['View Info 1', 'View Info 2']}>
+    <Para />
+    <Para />
+  </AccordionGroup>
+);
+
+export const DefaultExample = () => (
+  <AccordionGroup titles={['View Info 1', 'View Info 2']}>
+    <Para />
+    <Para />
+  </AccordionGroup>
+);
--- packages/storybook/stories/Accordion.stories.tsx
+++ packages/storybook/stories/Accordion.stories.tsx
@@ -1,11 +1,21 @@
 import { ComponentMeta } from '@storybook/react';
 import React from 'react';
-import {
-  CustomIcon,
-  Default,
-  Expanded,
-} from '../../documentation/components/accordion/widgets-variants';
 import { Accordion } from '../../lib/components';
+
+const Para = () => (
+  <p>
+    Sed laoreet neque eget sem varius, et interdum dui venenatis. Suspendisse in
+    faucibus tortor, nec aliquet arcu. Quisque at Proin auctor velit massa,
+    euismod pretium dui euismod in. Pellentesque rhoncus eros id posuere
+    tincidunt. Maecenas quis libero vitae elit consectetur finibus et ac libero.
+    Donec at fermentum lectus. Cras iaculis augue non mauris interdum, vitae
+    pretium mi blandit. Aenean ultrices pellentesque lectus ac faucibus. Morbi
+    tristique vulputate nisi, id porttitor diam egestas a. Suspendisse a tortor
+    suscipit, accumsan massa at, viverra urna. Maecenas vel lectus sodales,
+    dapibus dolor eget, pharetra neque. Nam eleifend id mauris in suscipit. Ut
+    sed risus at mi vulputate rhoncus.
+  </p>
+);
 
 export default {
   component: Accordion,
@@ -19,6 +29,25 @@ export default {
   title: 'Accordion',
 } as ComponentMeta<typeof Accordion>;
 
-export const CustomIconExample = () => <>{CustomIcon}</>;
-export const DefaultExample = () => <>{Default}</>;
-export const ExpandedExample = () => <>{Expanded}</>;
+export const CustomIconExample = () => (
+  <Accordion
+    title="View Info"
+    customIcon={
+      <i>
+        <img src="https://img.icons8.com/material-sharp/24/000000/plus--v1.png" />
+      </i>
+    }
+  >
+    <Para />
+  </Accordion>
+);
+export const DefaultExample = () => (
+  <Accordion title="View Info">
+    <Para />
+  </Accordion>
+);
+export const ExpandedExample = () => (
+  <Accordion expanded title="View Info">
+    <Para />
+  </Accordion>
+);
