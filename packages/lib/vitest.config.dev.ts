6cd7cb587aca235207ae2023bd071afff18a5f65
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-06-16 22:00:39 +0530

    update vitest config

--- packages/lib/vitest.config.dev.ts
+++ packages/lib/vitest.config.dev.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
