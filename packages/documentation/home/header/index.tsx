1f065fe7f22316e53ea16a8d2c55b1bbdbac2407
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-07-11 23:14:08 +0530

    doc search integration

--- packages/documentation/home/header/index.tsx
+++ packages/documentation/home/header/index.tsx
@@ -1,3 +1,6 @@
+// import React from 'react';
+import { DocSearch } from '@docsearch/react';
+import React from 'react';
 import { AutoSuggestOption } from '../../../lib/components/auto-suggest/auto-suggest.model';
 import { Badge } from '../../common/github-link';
 import { AppSettings } from '../app-settings';
@@ -16,11 +19,15 @@ const Header: React.FC<{
       </div>
       <Badge label="alpha" />
       {/* <ThemeSwitcher /> */}
-      {/* {!isMobile && (
-        <div className="algolia-container" style={{ width: '200px' }}>
-          <AlgoliaSearch onSelection={onSearchSelection} />
+      {!isMobile && (
+        <div className="algolia-container" style={{ marginLeft: 'auto' }}>
+          <DocSearch
+            apiKey="265c5def59c32bb59531e98e01c9537d"
+            appId="UZV2STWBAE"
+            indexName="react-creme"
+          />
         </div>
-      )} */}
+      )}
       <AppSettings />
     </header>
   );
