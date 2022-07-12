cfe1688935a16bf73e7da7b5879e7f4fc0cc3f7c
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-07-13 00:32:42 +0530

    updating algolia ui

--- packages/documentation/home/header/index.tsx
+++ packages/documentation/home/header/index.tsx
@@ -1,11 +1,20 @@
 // import React from 'react';
 import { DocSearch } from '@docsearch/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { AutoSuggestOption } from '../../../lib/components/auto-suggest/auto-suggest.model';
 import { Badge } from '../../common/github-link';
 import { AppSettings } from '../app-settings';
 import { Logo } from '../logo';
 import './header.scss';
+
+const AlgoliaHit = ({ hit, children }: { children; hit: { url: string } }) => {
+  return (
+    <div className="algolia-hit" style={{ width: '100%' }}>
+      <Link to={hit.url.split('/').pop()}>{children}</Link>
+    </div>
+  );
+};
 
 const Header: React.FC<{
   isMobile?: boolean;
@@ -25,6 +34,7 @@ const Header: React.FC<{
             apiKey={import.meta.env.VITE_APP_ALGOLIA_API_KEY as string}
             appId={import.meta.env.VITE_APP_ALGOLIA_APP_ID as string}
             indexName={import.meta.env.VITE_APP_ALGOLIA_INDEX_NAME as string}
+            hitComponent={AlgoliaHit}
           />
         </div>
       )}
