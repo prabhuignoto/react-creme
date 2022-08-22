ae6bb59c57a207722bd5aee623eea0800992404c
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-08-22 11:11:53 +0530

    fix unit tests

--- packages/lib/vitest.config.dev.ts
+++ packages/lib/vitest.config.dev.ts
@@ -2,10 +2,17 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@design': path.resolve(__dirname, './design'),
+      '@icons': path.resolve(__dirname, './icons'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
--- packages/lib/vitest.config.ts
+++ packages/lib/vitest.config.ts
@@ -3,10 +3,17 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@design': path.resolve(__dirname, './design'),
+      '@icons': path.resolve(__dirname, './icons'),
+    },
+  },
   test: {
     coverage: {
       clean: true,
