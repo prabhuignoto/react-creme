257cd89e69a7f8bf823a7c9b345570d227fc5cc2
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-08-28 22:54:01 +0530

    fix views

--- packages/documentation/postcss.config.js
+++
@@ -1,8 +0,0 @@
-//postcss config
-import PostCSSImport from 'postcss-import';
-import PostCSSPresetEnv from 'postcss-preset-env';
-import PostCSSReporter from 'postcss-reporter';
-
-module.exports = {
-  plugins: [PostCSSImport(), PostCSSReporter(), PostCSSPresetEnv()],
-};
