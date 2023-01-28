fffddd6d400e5d0315cebd5744c7a07e5c337a9a
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-01-28 14:25:23 +0530

    update documentation (#233)

--- packages/documentation/home/index.tsx
+++ packages/documentation/home/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Text } from '../../lib/components';
+import { BlockQuote, Button, Text } from '../../lib/components';
 import { Section } from '../../lib/components/section/section';
 // import packages from '../../lib/package.json';
 import { SyntaxHighLighter } from './../common/syntax-highlighter';
@@ -20,11 +20,28 @@ const Home: React.FunctionComponent = () => {
         {/* SETUP */}
         <Section title="Installation" size="md" hashPrefix="home">
           <Text>
-            Get started by installing the <em>react-creme</em> package via npm
-            or yarn.
+            Getting started with the <em>react-creme</em> package is easy. To
+            install it, you can use either npm or yarn package manager. If you
+            prefer to use npm, you can install it by running the following
+            command in your terminal:
           </Text>
           <Code wrap={false}>{`npm install --save react-creme`}</Code>
+          <Text>
+            If you prefer to use yarn, you can install it by running the
+            following command in your terminal:
+          </Text>
           <Code wrap={false}>{`yarn add react-creme`}</Code>
+          <Text>
+            After the installation is complete, you can import the package and
+            use its components and functionalities in your React application.
+          </Text>
+
+          <BlockQuote>
+            Once the package is installed, you can start building your
+            application with the powerful set of features provided by
+            react-creme, such as the ThemeProvider, which allows you to
+            customize the visual design of your application with ease.
+          </BlockQuote>
         </Section>
 
         <Section title="Usage" size="md" hashPrefix="home">
@@ -47,8 +64,20 @@ const Home: React.FunctionComponent = () => {
 
         <Section title="Theme" size="md" hashPrefix="home">
           <Text>
-            <em>react-creme</em> comes wit a powerful <em>ThemeProvider</em>{' '}
-            wrapper to customize the colors, font , icon sizes with ease.
+            The <em>react-creme</em> library comes with a powerful ThemeProvider
+            wrapper that makes it easy to customize the visual elements of your
+            application. This wrapper allows you to easily change the colors,
+            fonts, and icon sizes throughout your application with a single
+            point of configuration. This can be useful in situations where you
+            need to update the visual design of your application or when you
+            want to create a consistent look and feel across multiple pages or
+            components.
+          </Text>
+          <Text>
+            The ThemeProvider wrapper is easy to use and can be integrated into
+            your application in just a few lines of code, making it a powerful
+            tool for customizing the visual design of your React projects with
+            ease.
           </Text>
           <Text>
             The following example demonstrates how to use the{' '}
