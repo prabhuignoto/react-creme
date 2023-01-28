3aad18b6c23bdb1b196637d6eaaa30de7d81b1a1
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-01-28 12:35:17 +0530

    updating vitest config

--- packages/lib/test-setup.ts
+++ packages/lib/test-setup.ts
@@ -2,3 +2,9 @@ import '@testing-library/jest-dom/extend-expect';
 import ResizeObserver from 'resize-observer-polyfill';
 
 global.ResizeObserver = ResizeObserver;
+
+process.on('unhandledRejection', reason => {
+  // eslint-disable-next-line no-console
+  console.log(`FAILED TO HANDLE`);
+  throw reason;
+});
