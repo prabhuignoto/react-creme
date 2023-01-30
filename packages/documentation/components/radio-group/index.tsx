003fe00fea2123f00f56aea8a51bd7cd5b4a05d4
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-01-30 15:31:15 +0530

    fix radio group

--- packages/documentation/components/radio-group/index.tsx
+++ packages/documentation/components/radio-group/index.tsx
@@ -4,14 +4,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import DemoPageRenderer from '../../common/demo-page-renderer';
 import Widgets from './widgets';
 
+const Description = (
+  <div>
+    <p>
+      A Radio group is a UI component that allows users to select one option
+      from a list of mutually exclusive options. It consists of a series of
+      radio buttons, each associated with a label, and only one button can be
+      selected at a time.{' '}
+    </p>
+    <p>
+      The purpose of a radio group is to provide a clear and intuitive way for
+      users to make a single selection from a list of options.
+    </p>
+  </div>
+);
+
 function radioGroup() {
   return (
     <DemoPageRenderer
       demoWidget={<Widgets />}
       title="Radio Group"
-      description={`Radio buttons are an essential element of forms.
-      They are used when there is a list of two or more options that are mutually exclusive and the user must select exactly one choice.
-      Clicking a non-selected radio button will deselect whatever other button was previously selected in the list.`}
+      description={Description}
       tabTitles={['Examples', 'Properties', 'Stackblitz']}
       stackBlitzCodes={['react-ts-m9yskx']}
       pageIcon={<FontAwesomeIcon icon={faTasks} size="2x" />}
