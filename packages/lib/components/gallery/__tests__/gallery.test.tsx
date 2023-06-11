0da6ba3b3f970b7a0ffd9bfc241ac50704174547
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2023-06-11 17:34:23 +0530

    fix unit test cases

--- packages/lib/components/gallery/__tests__/gallery.test.tsx
+++ packages/lib/components/gallery/__tests__/gallery.test.tsx
@@ -1,6 +1,5 @@
 import { render } from '@testing-library/react';
-import { Image } from 'react-creme';
-
+import { Image } from '../../image/image';
 import { Gallery } from '../gallery';
 
 describe('Gallery', () => {
@@ -23,6 +22,14 @@ describe('Gallery', () => {
           src="https://mmc.tirto.id/image/otf/500x0/2016/07/26/TIRTO-20140522_batman_warner-bros_ratio-16x9.jpg"
           alt="test_image_4"
         />
+        <Image
+          src="https://mmc.tirto.id/image/otf/500x0/2016/07/26/TIRTO-20140522_batman_warner-bros_ratio-16x9.jpg"
+          alt="test_image_5"
+        />
+        <Image
+          src="https://mmc.tirto.id/image/otf/500x0/2016/07/26/TIRTO-20140522_batman_warner-bros_ratio-16x9.jpg"
+          alt="test_image_5"
+        />
       </Gallery>
     );
     expect(getByAltText('test_image_1')).toBeInTheDocument();
@@ -35,7 +42,7 @@ describe('Gallery', () => {
 
     expect(
       (container.firstChild as HTMLElement).querySelectorAll('img')
-    ).toHaveLength(4);
+    ).toHaveLength(6);
   });
 
   it("should render all the imageURL's", () => {
