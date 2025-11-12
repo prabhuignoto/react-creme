5efdcae1280aefebe3d36a8611177a7e823978a4
prabhuignoto <prabhu.m.murthy@gmail.com>
2025-11-12 19:18:53 +0530

    Add draggable widgets component for enhanced documentation examples

---
+++ packages/documentation/components/draggable/widgets.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/**
+ * Draggable widgets component
+ */
+const Widgets: React.FC = () => {
+  return (
+    <div>
+      <p>Draggable component examples will be rendered here.</p>
+    </div>
+  );
+};
+
+Widgets.displayName = 'Widgets';
+
+export default Widgets;
